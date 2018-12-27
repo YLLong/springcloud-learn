4c0921f5a3506d45241d15512f5f333925591a75
yYS1778 <YS.yanlinlong@h3c.com>
2018-12-27 16:57:35 +0800

    init

---
+++ ch3-2/ch3-2-eureka-client/src/main/java/com/yll/Ch32EurekaClientApplication.java
@@ -0,0 +1,16 @@
+package com.yll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Ch32EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch32EurekaClientApplication.class, args);
+    }
+
+}
+
