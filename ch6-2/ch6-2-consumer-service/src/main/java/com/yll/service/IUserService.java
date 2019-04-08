f082e47bc683c1f6aa8ba3b72f9dfee6a1c02662
yYS1778 <YS.yanlinlong@h3c.com>
2019-04-09 07:47:49 +0800

    测通示列

--- ch6-2/ch6-2-consumer-service/src/main/java/com/yll/service/IUserService.java
+++ ch6-2/ch6-2-consumer-service/src/main/java/com/yll/service/IUserService.java
@@ -4,10 +4,11 @@ import com.yll.service.impl.UserServiceFallBack;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @className: IUserService
- * @description: //TODO
+ * @description: //feign 客户端调用服务
  * @author: yys1778
  * @date: Created in 2019/4/8 17:22
  * @modify by: yys1778
@@ -16,7 +17,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(name = "sc-provider-service", fallback = UserServiceFallBack.class)
 public interface IUserService {
 
+    /**
+     * @title: getUser
+     * @description: // get 请求 @requestParam 注解不能少
+     * @author: yys1778
+     * @date: Created in 2019/4/9 7:45
+     * @param userName
+     * @throws:
+     * @return: java.lang.String
+     */
     @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public String getUser(String userName);
+    public String getUser(@RequestParam("userName") String userName);
 
 }
